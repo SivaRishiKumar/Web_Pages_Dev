3cf610233c37031e6b714c15cab50158c1345889
SivaRishiKumar <sakesivakumar7799@gmail.com>
2024-05-03 00:12:08 +0530

    commiting all files

---
+++ rayroman.js
@@ -0,0 +1,11 @@
+let hover = document.querySelector("#hover")
+console.log(hover);
+
+for(let i=0; i<=hover.length; i++){
+     console.log(hover[i]);
+
+
+// hover[i].onmouseover = function(){
+       hover[i].style.color = "red"
+// }
+}
